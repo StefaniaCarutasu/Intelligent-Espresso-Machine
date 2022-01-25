31ed8a8d421954809c24579df5baa68aa4de517b
Stefania Carutasu <62119841+StefaniaCarutasu@users.noreply.github.com>
2022-01-25 18:04:00 +0200

    updated database with tables for coffee types

---
+++ rasputin/coffee.py
@@ -0,0 +1,11 @@
+class BeverageType:
+    def __init__(self, name, coffee_quantity, milk_quantity, milk_froth, roast_type, flavour):
+        self.name = name
+        self.coffee_quantity = coffee_quantity
+        self.milk_quantity = milk_quantity
+        self.milk_froth = milk_froth
+        self.added_flavour = flavour
+        self.roast_type = roast_type
+
+    def changeCaffeineQuantity(self, caffeine_quantity):
+        self.coffee_quantity = caffeine_quantity
--- rasputin/schema.sql
+++ rasputin/schema.sql
@@ -8,3 +8,55 @@ CREATE TABLE user (
   remember BOOLEAN
 );
 
+
+DROP TABLE IF EXISTS beverages_types;
+
+CREATE TABLE beverages_types (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL,
+    coffee_quantity INTEGER NOT NULL,              -- ml of espresso
+    milk_quantity INTEGER NOT NULL,                -- ml of milk
+    milk_froth BOOLEAN NOT NULL,
+    coffee_type TEXT NOT NULL                      -- hot or cold
+);
+
+
+DROP TABLE IF EXISTS user_preference;
+
+CREATE TABLE user_preference (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER UNIQUE ,
+    beverage_id INTEGER,
+    roast_type TEXT NOT NULL,
+    flavour_type TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (beverage_id) REFERENCES beverages_types(id)
+);
+
+
+DROP TABLE IF EXISTS preprogrammed_coffee;
+
+CREATE TABLE preprogrammed_coffee (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    beverage_id INTEGER,
+    roast_type TEXT NOT NULL,
+    flavour_type TEXT,
+    start_time TIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (beverage_id) REFERENCES beverages_types(id)
+);
+
+
+DROP TABLE IF EXISTS machine_state;
+
+CREATE TABLE machine_state (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    coffee_quantity INTEGER NOT NULL,
+    milk_quantity INTEGER NOT NULL,
+    syrup_quantity INTEGER NOT NULL,
+    broken BOOLEAN NOT NULL
+);
+
+
+
