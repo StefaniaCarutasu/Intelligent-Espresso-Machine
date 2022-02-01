6dd974868bfc3ccf396992effe3d333cfc4f6cb8
Claudia Dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2022-02-01 21:48:43 +0200

    test api

--- tests/unit/webapp/test_http.py
+++ tests/unit/webapp/test_http.py
@@ -1,13 +1,14 @@
 from rasputin.__init__ import create_app
 from flask_testing import TestCase
+import json
 
-username = 'admin'
+username = 'claudia_'
 email = 'admin@gmail.com'
 password = 'admin1234'
 confirm_password = 'admin1234'
 
-username_login = 'user'
-password_login = 'user1234'
+username_login = 'claudia_maria_7'
+password_login = 'test1234'
 
 beverage_type = 'Espresso'
 roast_type = 'Medium'
@@ -296,4 +297,35 @@ class RasputinTestCase(TestCase):
         assert res.status_code == 200
 
 
+class RasputinApiTestCase(TestCase):
+    def create_app(self):
+        return create_app({'TESTING': True})
 
+    def login(self, username, password):
+            return self.client.post('/auth/api/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+
+    def test_login(self):
+        # post wrong username
+        res = self.login(f"{username_login}x", password_login)
+        json_res = json.loads(res.data.decode())
+
+        assert res.status_code == 403
+        assert json_res['status'] == 'Incorrect username.'
+
+        # post wrong password
+        res = self.login(username_login, f"{password_login}x")
+        json_res = json.loads(res.data.decode())
+
+        assert res.status_code == 403
+        assert json_res['status'] == 'Incorrect password.'
+
+        # post 
+        res = self.login(username_login, password_login)
+        json_res = json.loads(res.data.decode())
+
+        assert res.status_code == 200
+        assert json_res['status'] == 'Logged in succesfully!'
